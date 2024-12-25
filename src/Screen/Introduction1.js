2e4a60267d83ab8384386d019f26010f4343f792
rakeshcv90 <rakeshrao@cvinfotech.com>
2024-12-25 10:34:08 +0530

    RaoUpdate

---
+++ src/Screen/Introduction1.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Introduction1 = () => {
+  return (
+    <View>
+      <Text>Introduction1</Text>
+    </View>
+  )
+}
+
+export default Introduction1
---
+++ src/Screen/SplaceScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SplaceScreen = () => {
+  return (
+    <View>
+      <Text>SplaceScreen</Text>
+    </View>
+  )
+}
+
+export default SplaceScreen
